fa8f223e244e09d8802cf1a175b3bca11d6e086e
almeidabbm <almeidabatistam@gmail.com>
2023-11-14 12:20:23 +0000

    fix: allow for async actions

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -2,7 +2,6 @@ import { Command, Option } from 'commander';
 import path from 'path';
 
 const scanCommand = new Command();
-
 scanCommand
   .name('scan')
   .description('scan a repository and generate least priviledge policies')
@@ -15,7 +14,7 @@ scanCommand
       .makeOptionMandatory(true)
   )
   .argument('<path>', 'repository path')
-  .action((pathArg, { cloudProvider }) => {
+  .action(async (pathArg, { cloudProvider }) => {
     const fullPath = path.resolve(process.cwd(), pathArg);
     console.log({ fullPath, cloudProvider });
   });
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import { Command } from 'commander';
 import scanCommand from './commands/scan';
 
 const program = new Command();
-program.name('slauth').addCommand(scanCommand).parse(process.argv);
+program.name('slauth').addCommand(scanCommand).parseAsync(process.argv);
