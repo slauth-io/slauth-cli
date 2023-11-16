e66fe7647e0bd801f59a02ab78b1188637bee90d
almeidabbm <almeidabatistam@gmail.com>
2023-11-16 15:26:02 +0000

    fix: enforce all cloudproviders to have prompts

--- src/utils/prompts/index.ts
+++ src/utils/prompts/index.ts
@@ -5,16 +5,20 @@ import {
   AWS_GENERATE_POLICIES_PROMPT,
 } from './aws';
 
+// Make prompts immutable, cannog assign prompts.aws = {} will error
 type Prompts = {
-  [k in CloudProviders]: {
+  readonly [k in CloudProviders]: {
     DETECT_STATEMENTS_PROMPT: ChatPromptTemplate;
     GENERATE_POLICIES_PROMPT: ChatPromptTemplate;
   };
 };
 
-export default {
+// Enforce all cloudproviders to have prompts
+const cpPrompts: Prompts = {
   aws: {
     DETECT_STATEMENTS_PROMPT: AWS_DETECT_STATEMENTS_PROMPT,
     GENERATE_POLICIES_PROMPT: AWS_GENERATE_POLICIES_PROMPT,
   },
-} as Prompts;
+};
+
+export default cpPrompts;
