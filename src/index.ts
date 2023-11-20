98adf85dd0d151c0a5c9d448dad4de5fafc64747
almeidabbm <almeidabatistam@gmail.com>
2023-11-20 13:19:32 +0000

    feat: add version option

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import 'dotenv/config';
 import { Command } from 'commander';
 import scanCommand from './commands/scan';
 import { coloredLogo } from './utils/print-ascii-logo';
+import { version } from '../package.json';
 
 const program = new Command();
 program
   .name('slauth')
   .addCommand(scanCommand)
   .addHelpText(`beforeAll`, coloredLogo)
+  .version(version)
   .parseAsync(process.argv);
