be92662a54ce36e2cc139b5f3d1af40795735068
almeidabbm <almeidabatistam@gmail.com>
2023-11-20 15:20:47 +0000

    chore: remove debugging info from update-version

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,10 +1,2 @@
 NODE_VERSION=$(node -p -e "require('./package.json').version")
 echo "export default '${NODE_VERSION}';" > src/version.ts
-
-echo 'UPDATING VERSION'
-
-cat src/version.ts
-
-pwd
-
-ls
