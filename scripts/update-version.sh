abec052a13ad6f12c633875b84ae97f4a4687614
almeidabbm <almeidabatistam@gmail.com>
2023-11-20 15:17:35 +0000

    fix: debug prepack

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,2 +1,10 @@
 NODE_VERSION=$(node -p -e "require('./package.json').version")
 echo "export default '${NODE_VERSION}';" > src/version.ts
+
+echo 'UPDATING VERSION'
+
+cat src/version.ts
+
+pwd
+
+ls
